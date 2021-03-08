f9cad9bf735c6e197573dac2405038db5ab159fd
melissa-koi <melissawangui3@gmail.com>
2021-03-08 07:38:21 +0300

    can output to console added item to cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-let carts = document.querySelectorAll(".cartRef, input[type=radio]:checked, input:checked")
-var checked = form.querySelector("");
-var checked2 = document.querySelectorAll('');
+
+let checked = $("button#submit").click(function(event){
+    event.preventDefault();
+    if(document.querySelectorAll("input[type=radio]:checked")) {
+        console.log("added to cart")
+    }
+});
+
+let carts = document.querySelectorAll(".cartRef")
+
+
+for(let i=0; i<carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        console.log("added to cart")
+    })
+}
+ function cartNUmbers() {
+     let productNumbers = localStorage.getItem('cartNumbers')
+     localStorage.setItem('cartNumbers',1)
+ }
