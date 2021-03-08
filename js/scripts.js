47c6e16b28018151c79211c0d608fad2546e5d1c
melissa-koi <melissawangui3@gmail.com>
2021-03-08 08:47:16 +0300

    add object for chefs fav

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,73 @@
 let checked = $("button#submit").click(function(event){
     event.preventDefault();
     if(document.querySelectorAll("input[type=radio]:checked,input[type=checkbox]")) {
-        cartNumbers()
+        console.log(products[0])
     }
 });
 
 let carts = document.querySelectorAll(".cartRef")
 for(let i=0; i<carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers()
+        console.log(products[i])
     })
 }
- function cartNumbers() {
+
+let products = [
+    {
+        name:'All Meats',
+        tag:'All Meats',
+        price:15,
+        inCart:0
+    },
+    {
+        name:'Hawaiian',
+        tag:'Hawaiian',
+        price:17,
+        inCart:0
+    },
+    {
+        name:'Margherita',
+        tag:'Margherita',
+        price:16,
+        inCart:0
+    },
+    {
+        name:'Pepperoni',
+        tag:'Pepperoni',
+        price:15,
+        inCart:0
+    },
+    {
+        name:'3 Cheeses',
+        tag:'3 Cheeses',
+        price:12,
+        inCart:0
+    },
+    {
+        name:'Mexicana',
+        tag:'Mexicana',
+        price:14,
+        inCart:0
+    },
+    {
+        name:'Vegetarian',
+        tag:'Vegetarian',
+        price:12,
+        inCart:0
+    }
+]
+
+function cartNumbers() {
      let productNumbers = localStorage.getItem('cartNumbers');
-     console.log(productNumbers)
-     localStorage.setItem('cartNumbers',1)
- }
+     productNumbers = parseInt(productNumbers);
+
+     if(productNumbers) {
+         localStorage.setItem('cartNumbers',productNumbers+1)
+         document.querySelector('.cart span').textContent=productNumbers+1
+     }else{
+        localStorage.setItem('cartNumbers',1)
+        document.querySelector('#navBar span').textContent=1
+    }
+}
+
+     
